0679460fb30e127a0fd951c682b17e089f62a834
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2024-04-25 03:10:35 +0700

    fix: change folder secrets to keys

--- utils/telegram_bot.py
+++ utils/telegram_bot.py
@@ -1,8 +1,5 @@
 import requests
-
-BOT_TOKEN = '7130840623:AAGQ-IXQ5qI3SK3BfiVVi7MdEztrOA_N4-Q'
-BOT_CHATID = '1286703400'
-
+from utils.token import BOT_CHATID, BOT_TOKEN
 
 def send_message(bot_message: str):
     send_text = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={BOT_CHATID}&parse_mode=Markdown&text={bot_message}'
